f0b0ca962699bae7e36bb4bff3d6637729b3eb4c
guaitafranco <guaita.franco@gmail.com>
2024-11-18 23:10:06 -0300

    update logos liga argentina.

---
+++ updateLogosARG.sql
@@ -0,0 +1,67 @@
+UPDATE "Team" 
+SET "logo" = CASE
+    WHEN "id" = 'sr:competitor:3204' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Escudo_club_Atl%C3%A9tico_Uni%C3%B3n_de_santa_fe.svg/512px-Escudo_club_Atl%C3%A9tico_Uni%C3%B3n_de_santa_fe.svg.png'
+    WHEN "id" = 'sr:competitor:3211' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Escudo_del_C_A_River_Plate.svg/129px-Escudo_del_C_A_River_Plate.svg.png'
+    WHEN "id" = 'sr:competitor:3208' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Escudo_del_Club_Atl%C3%A9tico_V%C3%A9lez_Sarsfield.svg/140px-Escudo_del_Club_Atl%C3%A9tico_V%C3%A9lez_Sarsfield.svg.png'
+    WHEN "id" = 'sr:competitor:3206' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Escudo_de_Estudiantes_de_La_Plata.svg/125px-Escudo_de_Estudiantes_de_La_Plata.svg.png'
+    WHEN "id" = 'sr:competitor:191648' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Escudo_del_Club_Deportivo_Riestra.svg/138px-Escudo_del_Club_Deportivo_Riestra.svg.png'
+    WHEN "id" = 'sr:competitor:3205' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Club-de-gimnasia-y-esgrima-la-plata-escudo.png/160px-Club-de-gimnasia-y-esgrima-la-plata-escudo.png'
+    WHEN "id" = 'sr:competitor:3215' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Escudo_de_Racing_Club_%282014%29.svg/132px-Escudo_de_Racing_Club_%282014%29.svg.png'
+    WHEN "id" = 'sr:competitor:7629' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Emblema_oficial_del_Club_Atl%C3%A9tico_Hurac%C3%A1n.svg/98px-Emblema_oficial_del_Club_Atl%C3%A9tico_Hurac%C3%A1n.svg.png'
+    WHEN "id" = 'sr:competitor:3209' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Escudo_del_Club_Atl%C3%A9tico_Independiente.svg/152px-Escudo_del_Club_Atl%C3%A9tico_Independiente.svg.png'
+    WHEN "id" = 'sr:competitor:3212' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Escudo_del_Club_Atl%C3%A9tico_Newell%27s_Old_Boys_de_Rosario.svg/130px-Escudo_del_Club_Atl%C3%A9tico_Newell%27s_Old_Boys_de_Rosario.svg.png'
+    WHEN "id" = 'sr:competitor:3203' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Escudo_Oficial_del_Club_Atl%C3%A9tico_Belgrano.png/190px-Escudo_Oficial_del_Club_Atl%C3%A9tico_Belgrano.png'
+    WHEN "id" = 'sr:competitor:6074' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Logo_of_CD_Godoy_Cruz_Antonio_Tomba.png/161px-Logo_of_CD_Godoy_Cruz_Antonio_Tomba.png'
+    WHEN "id" = 'sr:competitor:65668' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Barracas_central_logo.svg/124px-Barracas_central_logo.svg.png'
+    WHEN "id" = 'sr:competitor:3201' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Escudo_del_Club_Atl%C3%A9tico_San_Lorenzo_de_Almagro.svg/160px-Escudo_del_Club_Atl%C3%A9tico_San_Lorenzo_de_Almagro.svg.png'
+    WHEN "id" = 'sr:competitor:3218' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Escudo_de_Lan%C3%BAs.14.png/150px-Escudo_de_Lan%C3%BAs.14.png'
+    WHEN "id" = 'sr:competitor:36837' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Escudo_del_Club_Alt%C3%A9tico_Platense.svg/127px-Escudo_del_Club_Alt%C3%A9tico_Platense.svg.png'
+    WHEN "id" = 'sr:competitor:3217' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Rosario_Central_shield.jpg/250px-Rosario_Central_shield.jpg'
+    WHEN "id" = 'sr:competitor:3216' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Asociacion_Atletica_Argentinos_Juniors.svg/160px-Asociacion_Atletica_Argentinos_Juniors.svg.png'
+    WHEN "id" = 'sr:competitor:36842' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Escudo_del_Club_Independiente_Rivadavia.svg/159px-Escudo_del_Club_Independiente_Rivadavia.svg.png'
+    WHEN "id" = 'sr:competitor:36839' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Escudo_del_Club_Social_y_Deportivo_Defensa_y_Justicia.svg/160px-Escudo_del_Club_Social_y_Deportivo_Defensa_y_Justicia.svg.png'
+    WHEN "id" = 'sr:competitor:7628' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Escudo_del_Club_Atl%C3%A9tico_Tigre_-_2019.svg/125px-Escudo_del_Club_Atl%C3%A9tico_Tigre_-_2019.svg.png'
+    WHEN "id" = 'sr:competitor:3210' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Escudo_Talleres_2015.svg/154px-Escudo_Talleres_2015.svg.png'
+    WHEN "id" = 'sr:competitor:4937' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Escudo_Instituto_Atletico_Central_Cordoba.png/157px-Escudo_Instituto_Atletico_Central_Cordoba.png'
+    WHEN "id" = 'sr:competitor:3219' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/CA_Banfield_%282014%29.svg/98px-CA_Banfield_%282014%29.svg.png'
+    WHEN "id" = 'sr:competitor:65676' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Escudo_del_Club_Central_C%C3%B3rdoba_de_Santiago_del_Estero.svg/150px-Escudo_del_Club_Central_C%C3%B3rdoba_de_Santiago_del_Estero.svg.png'
+    WHEN "id" = 'sr:competitor:36833' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Escudo_del_Club_Atletico_Tucuman.svg/136px-Escudo_del_Club_Atletico_Tucuman.svg.png'
+    WHEN "id" = 'sr:competitor:42338' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Escudo_del_Club_Atl%C3%A9tico_Sarmiento_de_Jun%C3%ADn.svg/163px-Escudo_del_Club_Atl%C3%A9tico_Sarmiento_de_Jun%C3%ADn.svg.png'
+    WHEN "id" = 'sr:competitor:3202' THEN 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/CABJ70.png/138px-CABJ70.png'
+    
+    ELSE logo
+END
+WHERE "id" IN ('sr:competitor:3204',
+'sr:competitor:3211',
+'sr:competitor:3208',
+'sr:competitor:3206',
+'sr:competitor:191648',
+'sr:competitor:3205',
+'sr:competitor:3215',
+'sr:competitor:7629',
+'sr:competitor:3209',
+'sr:competitor:3212',
+'sr:competitor:3203',
+'sr:competitor:6074',
+'sr:competitor:65668',
+'sr:competitor:3201',
+'sr:competitor:3218',
+'sr:competitor:36837',
+'sr:competitor:3217',
+'sr:competitor:3216',
+'sr:competitor:36842',
+'sr:competitor:36839',
+'sr:competitor:7628',
+'sr:competitor:3210',
+'sr:competitor:4937',
+'sr:competitor:3219',
+'sr:competitor:65676',
+'sr:competitor:36833',
+'sr:competitor:42338',
+'sr:competitor:3202');
+
+
+-- SELECT * FROM "Team"
+-- INNER JOIN "Standings" ON "Standings"."teamId" = "Team"."id"
+-- WHERE "Standings"."competitionId" = 'sr:season:114317';
+
